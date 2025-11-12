2a3614ab10e0e2ff47874c3ed3598d313ec8743a
Vansh Yadav <vansh.15926@gmail.com>
2025-11-12 13:35:19 +0530

    aws deployment edits..

---
+++ instrumentation.ts
@@ -0,0 +1,3 @@
+// This file is used for Next.js instrumentation.
+// You can safely delete it if you don't have other instrumentation to add.
+// Learn more: https://nextjs.org/docs/app/building-your-application/optimizing/instrumentation
---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  experimental: {
+    ppr: "incremental",
+    // after: true,
+  },
+}
+
+export default nextConfig
